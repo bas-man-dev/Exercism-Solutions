54bb8f22ad72ce6721b989a6f9e0e4601e760592
Bas Man <73193602+bas-man-dev@users.noreply.github.com>
2023-12-23 19:02:50 +0000

    Ruby T-05: finished

--- ruby/amusement-park-improvements/attendee.rb
+++ ruby/amusement-park-improvements/attendee.rb
@@ -1,4 +1,7 @@
 class Attendee
+  # I would make the height initialized then change it later so it's more obvious what's being done!
+  attr_accessor :pass_id
+
   def initialize(height)
     @height = height
   end
@@ -14,14 +17,14 @@ class Attendee
   # Do not edit above methods, add your own methods below.
 
   def has_pass?
-    raise 'Please implement the Attendee#haspass? method'
+    @pass_id ? true : false
   end
 
   def fits_ride?(ride_minimum_height)
-    raise 'Please implement the Attendee#fits_ride? method'
+    @height >= ride_minimum_height ? true : false
   end
 
   def allowed_to_ride?(ride_minimum_height)
-    raise 'Please implement the Attendee#allowed_to_ride? method'
+    @pass_id && fits_ride?(ride_minimum_height) ? true : false
   end
 end
