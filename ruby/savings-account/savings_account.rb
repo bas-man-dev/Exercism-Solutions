47cc166df0cc546a658d03e213adb4fb1fde63e3
Bas Man <73193602+bas-man-dev@users.noreply.github.com>
2023-12-26 16:20:13 +0000

    Ruby Savings Account : Completed

--- ruby/savings-account/savings_account.rb
+++ ruby/savings-account/savings_account.rb
@@ -1,13 +1,32 @@
 module SavingsAccount
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    if balance < 0
+      3.213
+    elsif balance >= 0 && balance < 1000
+      0.5
+    elsif balance >= 1000 && balance < 5000
+      1.621
+    else
+      2.475
+    end
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    annual_change = SavingsAccount.interest_rate(balance) / 100 + 1
+    if balance < 0
+      balance *= -1
+      balance *= annual_change
+      return balance *= -1
+    end
+    balance *= annual_change
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
-    raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+    count = 0
+    while current_balance < desired_balance
+      current_balance = SavingsAccount.annual_balance_update(current_balance)
+      count += 1
+    end
+    count
   end
 end
