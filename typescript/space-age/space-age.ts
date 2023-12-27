36fac9d70f928c16185fb38657dd7d804357f9c1
Bas Man <73193602+bas-man-dev@users.noreply.github.com>
2023-12-27 19:42:21 +0000

    Typescript Space-age: complete

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,3 +1,18 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+const planets: Record<string, number> = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
 }
+
+const SECONDS_IN_A_YEAR = 31557600
+
+export function age(planet: string, seconds: number): number {
+
+  return Number((1 / (planets[planet]) * (seconds / SECONDS_IN_A_YEAR)).toFixed(2))
+}
+
