32cebab67e13187646ce5ae593acd0c48a50299c
Bas Man <73193602+bas-man-dev@users.noreply.github.com>
2023-12-29 15:38:11 +0000

    Typescript DnD Character: completed

--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -1,9 +1,47 @@
+const fourRolls = () => {
+  let score = 0;
+  let scores: number[] = [];
+
+  for (let i = 0; i < 4; i++) {
+    score = Math.floor(Math.random() * 6) + 1;
+    scores.push(score);
+  }
+  return scores;
+};
+
+const getStat = () => {
+  const initialThrows: number[] = fourRolls();
+  let stat = 0;
+  // Get the top 3 throws
+  initialThrows.sort((a, b) => a - b).shift();
+  // Add the throws
+  initialThrows.forEach((item) => (stat += item));
+  return stat;
+};
 export class DnDCharacter {
-  public static generateAbilityScore(): number {
-    throw new Error('Remove this statement and implement this function')
+  strength?: number;
+  dexterity?: number;
+  constitution?: number;
+  intelligence?: number;
+  wisdom?: number;
+  charisma?: number;
+  hitpoints?: number;
+
+  public constructor() {
+    this.strength = getStat();
+    this.dexterity = getStat();
+    this.constitution = getStat();
+    this.intelligence = getStat();
+    this.wisdom = getStat();
+    this.charisma = getStat();
+    this.hitpoints = Math.floor((this.constitution - 10) / 2) + 10
   }
 
+  public static generateAbilityScore(): number {
+    return getStat();
+  } // do I need this??
+
   public static getModifierFor(abilityValue: number): number {
-    throw new Error('Remove this statement and implement this function')
+    return Math.floor((abilityValue - 10) / 2);
   }
 }
