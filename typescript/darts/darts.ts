f9695c7ca1d304707c6d1e28ca206e65303527e2
Bas Man <73193602+bas-man-dev@users.noreply.github.com>
2023-12-30 13:26:31 +0000

    Typescript Darts: completed

--- typescript/darts/darts.ts
+++ typescript/darts/darts.ts
@@ -1,3 +1,14 @@
-export function score(x: unknown, y: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function score(x: number, y: number): number {
+  // x**2 + y**2 = radius**2
+  const pointsSquared: number = x * x + y * y;
+  // return from the inside out so we can use the less than argument
+  if (pointsSquared <= 1) {
+    return 10;
+  } else if (pointsSquared <= 25) {
+    return 5;
+  } else if (pointsSquared <= 100) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
